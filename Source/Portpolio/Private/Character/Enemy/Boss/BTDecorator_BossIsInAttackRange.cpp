6b0ad6d5db6286606ea19efa72613567129c76b8
Doyun <djawleh@naver.com>
2021-01-26 21:40:52 +0900

    01/26
    AttackDetect -> AttackDecorator 로 변경

--- Source/Portpolio/Private/Character/Enemy/Boss/BTDecorator_BossIsInAttackRange.cpp
+++ Source/Portpolio/Private/Character/Enemy/Boss/BTDecorator_BossIsInAttackRange.cpp
@@ -1,5 +1,4 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "BTService_BossAttackDetect.h"
+#include "../Public/Character/Enemy/Boss/BTDecorator_BossIsInAttackRange.h"
 
---
+++ Source/Portpolio/Public/Character/Enemy/Boss/BTDecorator_BossIsInAttackRange.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameSetting/Portpolio.h"
+
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_BossIsInAttackRange.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO_API UBTDecorator_BossIsInAttackRange : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+};
