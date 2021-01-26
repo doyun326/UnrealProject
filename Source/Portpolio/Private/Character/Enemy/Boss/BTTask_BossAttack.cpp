0a38b4f4b8d1deb66d75d2edcce3d9f1fbca3fc0
Doyun <djawleh@naver.com>
2021-01-26 22:37:47 +0900

    01.26
    BTTask_BossAttack 기본 로직 작성

--- Source/Portpolio/Private/Character/Enemy/Boss/BTTask_BossAttack.cpp
+++ Source/Portpolio/Private/Character/Enemy/Boss/BTTask_BossAttack.cpp
@@ -1,3 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "../Public/Character/Enemy/Boss/BTTask_BossAttack.h"
+
+UBTTask_BossAttack::UBTTask_BossAttack()
+{
+	bNotifyTick = true;
+}
+
+EBTNodeResult::Type UBTTask_BossAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	EBTNodeResult::Type Result = Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_BossAttack::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
+
+	FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+}
--- Source/Portpolio/Public/Character/Enemy/Boss/BTTask_BossAttack.h
+++ Source/Portpolio/Public/Character/Enemy/Boss/BTTask_BossAttack.h
@@ -14,5 +14,16 @@ UCLASS()
 class PORTPOLIO_API UBTTask_BossAttack : public UBTTaskNode
 {
 	GENERATED_BODY()
+
+public:
+	UBTTask_BossAttack();
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+private:
 	
+
 };
