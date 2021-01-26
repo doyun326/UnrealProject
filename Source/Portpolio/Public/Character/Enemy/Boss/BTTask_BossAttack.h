5cfb9d25705ea3d3e47ff5ce235ddede02d51e55
Doyun <djawleh@naver.com>
2021-01-26 22:29:09 +0900

    01/26
    BTTask_BossAttack 추가

---
+++ Source/Portpolio/Public/Character/Enemy/Boss/BTTask_BossAttack.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameSetting/Portpolio.h"
+
+#include "BehaviorTree/BTTaskNode.h"
+#include "BTTask_BossAttack.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO_API UBTTask_BossAttack : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+};
