2de1a928dc7a9dba58b000c8a88eb668c2bd11a1
Doyun <djawleh@naver.com>
2021-01-31 15:12:31 +0900

    01/31
    Stage02 Minion(Enemy)추가

---
+++ Source/Portpolio/Public/Character/Enemy/Minion/EnemyMinionCharacter.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameSetting/Portpolio.h"
+
+#include "Character/BaseCharacter.h"
+#include "EnemyMinionCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO_API AEnemyMinionCharacter : public ABaseCharacter
+{
+	GENERATED_BODY()
+	
+};
