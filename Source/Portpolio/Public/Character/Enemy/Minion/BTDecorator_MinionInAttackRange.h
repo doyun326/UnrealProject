b10b8198eb976fcfaa35695846bfc37a24ea25bb
Doyun <djawleh@naver.com>
2021-02-03 14:48:16 +0900

    02/03
    BTDecorator_MinionInAttackRange Class 추가

---
+++ Source/Portpolio/Public/Character/Enemy/Minion/BTDecorator_MinionInAttackRange.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameSetting/Portpolio.h"
+
+#include "BehaviorTree/BTDecorator.h"
+#include "BTDecorator_MinionInAttackRange.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO_API UBTDecorator_MinionInAttackRange : public UBTDecorator
+{
+	GENERATED_BODY()
+	
+};
