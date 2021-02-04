55249de3ed87ae47647787a1a1fefeca0188b98f
Doyun <djawleh@naver.com>
2021-02-04 13:22:47 +0900

    02/04
    BTService_IsMinionHit 생성

---
+++ Source/Portpolio/Public/Character/Enemy/Minion/BTService_IsMinionHit.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameSetting/Portpolio.h"
+
+#include "BehaviorTree/BTService.h"
+#include "BTService_IsMinionHit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO_API UBTService_IsMinionHit : public UBTService
+{
+	GENERATED_BODY()
+	
+};
