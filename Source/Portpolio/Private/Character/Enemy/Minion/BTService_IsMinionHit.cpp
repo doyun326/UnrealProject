fcc82cdf81e24b347bce0cb75ca78fc20df0475b
Doyun <djawleh@naver.com>
2021-02-04 13:30:11 +0900

    02/04
    BTService_IsMinionHit 작성

--- Source/Portpolio/Private/Character/Enemy/Minion/BTService_IsMinionHit.cpp
+++ Source/Portpolio/Private/Character/Enemy/Minion/BTService_IsMinionHit.cpp
@@ -1,4 +1,45 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "../Public/Character/Enemy/Minion/BTService_IsMinionHit.h"
+#include "../Public/Character/Enemy/Minion/MinionAIController.h"
+#include "../Public/Character/Enemy/Minion/EnemyMinionCharacter.h"
+#include "../Public/Character/Player/PlayerCharacter.h"
 
+#include "BehaviorTree/BlackboardComponent.h"
+#include "DrawDebugHelpers.h"
+
+UBTService_IsMinionHit::UBTService_IsMinionHit()
+{
+	NodeName = TEXT("MinionIsHitService");
+}
+
+void UBTService_IsMinionHit::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	auto myCharacter = Cast<AEnemyMinionCharacter>(OwnerComp.GetAIOwner()->GetPawn());
+	UWorld* World = myCharacter->GetWorld();
+
+	if (myCharacter == nullptr)
+	{
+		ABLOG(Error, TEXT("Nullptr : myCharacter_"));
+		return;
+	}
+
+	if (World == nullptr)
+	{
+		ABLOG(Error, TEXT("Nullptr : World"));
+		return;
+	}
+
+	bool bResult = myCharacter->GetIsHiting();
+
+	if (bResult)
+	{
+		OwnerComp.GetBlackboardComponent()->SetValueAsBool(AMinionAIController::isHitKey_, true);
+	}
+	else
+	{
+		OwnerComp.GetBlackboardComponent()->SetValueAsBool(AMinionAIController::isHitKey_, false);
+	}
+}
--- Source/Portpolio/Public/Character/Enemy/Minion/BTService_IsMinionHit.h
+++ Source/Portpolio/Public/Character/Enemy/Minion/BTService_IsMinionHit.h
@@ -15,4 +15,10 @@ class PORTPOLIO_API UBTService_IsMinionHit : public UBTService
 {
 	GENERATED_BODY()
 	
+public:
+	UBTService_IsMinionHit();
+
+	virtual void TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+private:
+
 };
