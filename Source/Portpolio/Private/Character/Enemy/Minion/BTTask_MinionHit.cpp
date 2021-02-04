6bae13a45a262a90cc77a984b997037b96cd02ef
Doyun <djawleh@naver.com>
2021-02-04 13:50:56 +0900

    02/04
    BTTask_MinionHit 작성

--- Source/Portpolio/Private/Character/Enemy/Minion/BTTask_MinionHit.cpp
+++ Source/Portpolio/Private/Character/Enemy/Minion/BTTask_MinionHit.cpp
@@ -2,3 +2,24 @@
 
 #include "../Public/Character/Enemy/Minion/BTTask_MinionHit.h"
 
+UBTTask_MinionHit::UBTTask_MinionHit()
+{
+	isHiting_ = false;
+}
+
+EBTNodeResult::Type UBTTask_MinionHit::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	EBTNodeResult::Type Result = Super::ExecuteTask(OwnerComp, NodeMemory);
+
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_MinionHit::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickTask(OwnerComp, NodeMemory, DeltaSeconds);
+
+	if (!isHiting_)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	}
+}
--- Source/Portpolio/Public/Character/Enemy/Minion/BTTask_MinionHit.h
+++ Source/Portpolio/Public/Character/Enemy/Minion/BTTask_MinionHit.h
@@ -15,4 +15,15 @@ class PORTPOLIO_API UBTTask_MinionHit : public UBTTaskNode
 {
 	GENERATED_BODY()
 
+public:
+	UBTTask_MinionHit();
+
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+protected:
+	virtual void TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds) override;
+
+private:
+	bool	isHiting_;
+
 };
