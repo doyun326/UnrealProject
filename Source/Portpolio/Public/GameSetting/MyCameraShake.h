ef097267276a2e6f5b5946e540c47c1fa1a19a97
Doyun <djawleh@naver.com>
2021-02-14 15:13:59 +0900

    02/14
    MyCameraShake 추가

---
+++ Source/Portpolio/Public/GameSetting/MyCameraShake.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameSetting/Portpolio.h"
+
+#include "Camera/CameraShake.h"
+#include "MyCameraShake.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO_API UMyCameraShake : public UCameraShake
+{
+	GENERATED_BODY()
+	
+};
