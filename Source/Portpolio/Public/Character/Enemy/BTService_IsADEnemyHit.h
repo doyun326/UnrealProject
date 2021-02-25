397296757942a309153d7caacf4eed918f83d2ba
Doyun <djawleh@naver.com>
2021-02-26 00:05:37 +0900

    02/25
    BTService_IsADEnemyHit 추가

---
+++ Source/Portpolio/Public/Character/Enemy/BTService_IsADEnemyHit.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameSetting/Portpolio.h"
+
+#include "BehaviorTree/BTService.h"
+#include "BTService_IsADEnemyHit.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO_API UBTService_IsADEnemyHit : public UBTService
+{
+	GENERATED_BODY()
+	
+};
