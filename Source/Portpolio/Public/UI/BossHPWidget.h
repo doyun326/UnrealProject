ec30ee8bfb256beb080a58e5c6bb3c6cb8a39200
Doyun <djawleh@naver.com>
2021-03-03 22:41:02 +0900

    03/03
    BossHPwidget 추가

---
+++ Source/Portpolio/Public/UI/BossHPWidget.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameSetting/Portpolio.h"
+
+#include "Blueprint/UserWidget.h"
+#include "BossHPWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO_API UBossHPWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
