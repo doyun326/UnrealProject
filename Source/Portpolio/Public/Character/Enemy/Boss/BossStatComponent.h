d093488b97b5edb741ba758db6548d182a72e057
Doyun <djawleh@naver.com>
2021-03-03 21:53:23 +0900

    03/03
    BossStatComponent 작성

--- Source/Portpolio/Public/Character/Enemy/Boss/BossStatComponent.h
+++ Source/Portpolio/Public/Character/Enemy/Boss/BossStatComponent.h
@@ -7,6 +7,8 @@
 #include "Components/ActorComponent.h"
 #include "BossStatComponent.generated.h"
 
+DECLARE_MULTICAST_DELEGATE(FOnHpChangeDelegate);
+DECLARE_MULTICAST_DELEGATE(FOnHpZeroDelegate);
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class PORTPOLIO_API UBossStatComponent : public UActorComponent
@@ -16,10 +18,26 @@ class PORTPOLIO_API UBossStatComponent : public UActorComponent
 public:	
 	UBossStatComponent();
 
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	void	SetNewLevel(int32 _newLevel);
+	void	SetHp(float _newHp);
+	void	SetDamage(float _newDamage);
+	float	GetDamage();
+	float	GetHpRatio();
+
+	FOnHpChangeDelegate onHpChanged_;
+	FOnHpZeroDelegate	onHpZero_;
+
 protected:
 	virtual void BeginPlay() override;
+	virtual void InitializeComponent() override;
 
-public:	
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-		
+private:
+	struct FMinionEnemyData* currentStatData_ = nullptr;
+
+	UPROPERTY(EditInstanceOnly, Category = Stat, Meta = (AllowPrivateAccess = true))
+		int32 level_;
+	UPROPERTY(Transient, VisibleInstanceOnly, Category = Stat, meta = (AllowPrivateAccess = true))
+		float currentHP_;
 };
