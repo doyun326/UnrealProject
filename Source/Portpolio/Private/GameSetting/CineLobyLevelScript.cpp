c3fde42132fc6c3160d55edb132d1d20e7a6a46f
Doyun <djawleh@naver.com>
2021-03-05 16:14:57 +0900

    03/05
    CineLobyLevelScript 완성

--- Source/Portpolio/Private/GameSetting/CineLobyLevelScript.cpp
+++ Source/Portpolio/Private/GameSetting/CineLobyLevelScript.cpp
@@ -1,4 +1,53 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "../Public/GameSetting/CineLobyLevelScript.h"
+#include "../Public/GameSetting/WarGameInstance.h"
 
+#include "Components/WidgetComponent.h"
+#include "LevelSequence/Public/LevelSequencePlayer.h"
+#include "LevelSequence/Public/LevelSequence.h"
+#include "DrawDebugHelpers.h"
+
+#define CINELOBY_SEQUENCE_PATH "/Game/My/Cinematics/StageLoby/StageLoby_Master.StageLoby_Master"
+
+ACineLobyLevelScript::ACineLobyLevelScript()
+{
+
+}
+
+void ACineLobyLevelScript::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FStringAssetReference SequenceName(TEXT(CINELOBY_SEQUENCE_PATH));
+	ALevelSequenceActor* currentLevelSequenceActor = nullptr;
+
+	warInstance_ = Cast<UWarGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
+
+	if (warInstance_ == nullptr)
+	{
+		ABLOG(Error, TEXT("Nullptr : WarInstance"));
+		return;
+	}
+
+	sequenceAsset_ = Cast<ULevelSequence>(SequenceName.TryLoad());
+	sequencePlayer_ = ULevelSequencePlayer::CreateLevelSequencePlayer(GetWorld(), sequenceAsset_, FMovieSceneSequencePlaybackSettings(), currentLevelSequenceActor);
+
+	if (sequenceAsset_ == nullptr || sequencePlayer_ == nullptr)
+	{
+		ABLOG(Error, TEXT("Error : sequence"));
+		return;
+	}
+
+	if (sequencePlayer_)
+	{
+		sequencePlayer_->Play();
+	}
+
+	GetWorld()->GetTimerManager().SetTimer(startTimeHandler_, this, &ACineLobyLevelScript::NextLevel, 8.3f, false);
+}
+
+void ACineLobyLevelScript::NextLevel()
+{
+	UGameplayStatics::OpenLevel(this, FName("SpaceShip_Loby"));
+}
--- Source/Portpolio/Public/GameSetting/CineLobyLevelScript.h
+++ Source/Portpolio/Public/GameSetting/CineLobyLevelScript.h
@@ -15,4 +15,27 @@ class PORTPOLIO_API ACineLobyLevelScript : public ALevelScriptActor
 {
 	GENERATED_BODY()
 	
+public:
+	ACineLobyLevelScript();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void	NextLevel();
+
+	UPROPERTY()
+		class UWarGameInstance* warInstance_;
+	UPROPERTY()
+		class ULevelSequencePlayer* sequencePlayer_;
+	UPROPERTY(EditAnywhere)
+		class ULevelSequence* sequenceAsset_;
+	UPROPERTY(VisibleAnywhere, Category = "Trigger", meta = (AllowPrivateAccess = true))
+		UBoxComponent* walkTrigger_;
+	UPROPERTY(VisibleAnywhere, Category = "Trigger", meta = (AllowPrivateAccess = true))
+		UBoxComponent* mapFrontTrigger_;
+	UPROPERTY(VisibleAnywhere, Category = "Trigger", meta = (AllowPrivateAccess = true))
+		UBoxComponent* nextMapTrigger_;
+
+	FTimerHandle startTimeHandler_;
 };
