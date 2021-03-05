3b21632af35689d9a1f4a877e0d26d6a7128af22
Doyun <djawleh@naver.com>
2021-03-05 15:28:42 +0900

    03/05
    CineLobyLevelScript 추가

---
+++ Source/Portpolio/Public/GameSetting/CineLobyLevelScript.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameSetting/Portpolio.h"
+
+#include "Engine/LevelScriptActor.h"
+#include "CineLobyLevelScript.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO_API ACineLobyLevelScript : public ALevelScriptActor
+{
+	GENERATED_BODY()
+	
+};
