6fe0076d02ae5f59ada31eb7aef2e1dc38674f72
Doyun <djawleh@naver.com>
2021-03-06 18:06:41 +0900

    03/06
    CineStage03Script 추가

---
+++ Source/Portpolio/Public/GameSetting/CineStage03Script.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameSetting/Portpolio.h"
+
+#include "Engine/LevelScriptActor.h"
+#include "CineStage03Script.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PORTPOLIO_API ACineStage03Script : public ALevelScriptActor
+{
+	GENERATED_BODY()
+	
+};
