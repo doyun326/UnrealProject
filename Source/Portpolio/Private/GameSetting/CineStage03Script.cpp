811b87b093e31e146891fa95c173bdfab89c4e02
Doyun <djawleh@naver.com>
2021-03-06 18:21:43 +0900

    03/06
    CineStage03Scrtip 완료

--- Source/Portpolio/Private/GameSetting/CineStage03Script.cpp
+++ Source/Portpolio/Private/GameSetting/CineStage03Script.cpp
@@ -1,4 +1,52 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "../Public/GameSetting/CineStage03Script.h"
+#include "../Public/GameSetting/WarGameInstance.h"
 
+#include "LevelSequence/Public/LevelSequencePlayer.h"
+#include "LevelSequence/Public/LevelSequence.h"
+
+#define STAGE03_SEQUENCE_PATH "/Game/My/Cinematics/CIneStage03/CineStage03_Master.CineStage03_Master"
+
+ACineStage03Script::ACineStage03Script()
+{
+
+}
+
+void ACineStage03Script::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FStringAssetReference SequenceName(TEXT(STAGE03_SEQUENCE_PATH));
+	ALevelSequenceActor* currentLevelSequenceActor = nullptr;
+
+	warInstance_ = Cast<UWarGameInstance>(UGameplayStatics::GetGameInstance(GetWorld()));
+
+	if (warInstance_ == nullptr)
+	{
+		ABLOG(Error, TEXT("Nullptr : WarInstance"));
+		return;
+	}
+
+	sequenceAsset_ = Cast<ULevelSequence>(SequenceName.TryLoad());
+	sequencePlayer_ = ULevelSequencePlayer::CreateLevelSequencePlayer(GetWorld(), sequenceAsset_, FMovieSceneSequencePlaybackSettings(), currentLevelSequenceActor);
+
+	if (sequenceAsset_ == nullptr || sequencePlayer_ == nullptr)
+	{
+		ABLOG(Error, TEXT("Error : sequence"));
+		return;
+	}
+
+	if (sequencePlayer_)
+	{
+		warInstance_->SetPlayingSequence(true);
+		sequencePlayer_->Play();
+	}
+	GetWorld()->GetTimerManager().SetTimer(openLevelTimer_, this, &ACineStage03Script::OpenLevel, 8.0f, false);
+}
+
+void ACineStage03Script::OpenLevel()
+{
+	warInstance_->SetPlayingSequence(false);
+	UGameplayStatics::OpenLevel(this, FName("Stage_03"));
+}
--- Source/Portpolio/Public/GameSetting/CineStage03Script.h
+++ Source/Portpolio/Public/GameSetting/CineStage03Script.h
@@ -15,4 +15,21 @@ class PORTPOLIO_API ACineStage03Script : public ALevelScriptActor
 {
 	GENERATED_BODY()
 	
+public:
+	ACineStage03Script();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	void	OpenLevel();
+
+	UPROPERTY()
+		class UWarGameInstance*		warInstance_;
+	UPROPERTY()
+		class ULevelSequencePlayer* sequencePlayer_;
+	UPROPERTY(EditAnywhere)
+		class ULevelSequence*		sequenceAsset_;
+
+	FTimerHandle openLevelTimer_;
 };
